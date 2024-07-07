46fc107268cb5f315f5f87546f8daff1143f6eda
Su-Yong <simssy2205@gmail.com>
2024-07-07 19:23:24 +0900

    feat: init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,36 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {browser: true, es2020: true},
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  ignorePatterns: ['dist', '.*rc.js', '*.config.js', '*.config.ts'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': ['warn', {
+      additionalHooks: '(useAtom|useSetAtom|useEffectEvent)',
+    }],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
-module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
   trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: true,
 };
+
+export default config;
