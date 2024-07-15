b4de94f515176df650780c79e5a00e4217f37271
Su-Yong <simssy2205@gmail.com>
2024-07-15 23:03:44 +0900

    feat(TextInput): add `TextInput`

--- src/ui/TextInput.tsx
+++ src/ui/TextInput.tsx
@@ -1,12 +1,24 @@
-import { TextInput as BaseTextInput, TextInputProps as BaseTextInputProps } from 'react-native';
+import { View, TextInput as BaseTextInput, TextInputProps as BaseTextInputProps } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
 
 import { TypographySheet } from '@/features/themes';
-
-import { useTypographyStyle } from './Typography';
 import { createStyle } from '@/features/utils';
 
+import type { JSX } from 'react/jsx-runtime';
+
+const useTextInputStyle = createStyle((theme, variant: keyof TypographySheet, { left = false, right = false }: { left?: boolean; right?: boolean; } = {}) => ({
+  flex: 1,
+  fontFamily: theme.typography[variant].fontFamily,
+  fontWeight: theme.typography[variant].fontWeight,
+  fontSize: theme.typography[variant].fontSize,
+  letterSpacing: theme.typography[variant].letterSpacing,
+  marginRight: right ? 8 : 0,
+  marginLeft: left ? 8 : 0,
+}));
 const useTextInputViewStyle = createStyle((theme) => ({
+  flexDirection: 'row',
+
   paddingHorizontal: 16,
   paddingVertical: 12,
   borderRadius: 8,
@@ -16,14 +28,23 @@ const useTextInputViewStyle = createStyle((theme) => ({
 
 export interface TextInputProps extends BaseTextInputProps {
   variant?: keyof TypographySheet;
+  icon?: JSX.Element;
+  onIconPress?: () => void;
 }
-export const TextInput = ({ variant = 'body1', ...props }: TextInputProps) => {
-  const textStyle = useTypographyStyle(variant);
+export const TextInput = ({ variant = 'body1', icon, onIconPress, ...props }: TextInputProps) => {
+  const textStyle = useTextInputStyle(variant, { right: !!icon });
   const viewStyle = useTextInputViewStyle();
 
   return (
     <Animated.View style={viewStyle}>
       <BaseTextInput {...props} style={[textStyle, props.style]} />
+      {!!icon && (
+        <BorderlessButton activeOpacity={0.5} onPress={onIconPress}>
+          <View accessible accessibilityRole={'button'}>
+          {icon}
+          </View>
+        </BorderlessButton>
+      )}
     </Animated.View>
   );
 };
