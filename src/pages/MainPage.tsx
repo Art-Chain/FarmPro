21a36843bacd66781b5259460e493422349605e4
Su-Yong <simssy2205@gmail.com>
2024-07-17 20:46:23 +0900

    feat(header): add `BaseHeader`

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,17 @@
+import { useMemo } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { ArticlePage, HomePage } from '@/pages/main';
 import { useTheme } from '@/features/themes';
 
+import { BaseHeader, Header } from './components';
+import { DebugPage } from './DebugPage';
+
 import HomeIcon from '@/assets/images/home.svg';
 import ArticleIcon from '@/assets/images/article.svg';
 import UserIcon from '@/assets/images/user.svg';
-
-import { Header } from './components';
-import { useMemo } from 'react';
-import { DebugPage } from '@/pages/DebugPage.tsx';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import BackIcon from '@/assets/images/back.svg';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,7 +23,7 @@ export const MainPage = () => {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-        header: () => header,
+        header: (props) => route.name === 'home' ? header : <BaseHeader {...props}/>,
         tabBarStyle: {
           height: 62 + insets.bottom,
           padding: 8,
@@ -39,6 +40,7 @@ export const MainPage = () => {
         tabBarActiveTintColor: theme.colors.primary.main,
         tabBarInactiveTintColor: 'gray',
         tabBarLabel: route.name === 'home' ? '홈' : route.name === 'article' ? '기록' : '마이팜프로',
+        headerLeft: () => <BackIcon />,
       })}
     >
       <Tab.Screen name={'home'} component={HomePage}/>
