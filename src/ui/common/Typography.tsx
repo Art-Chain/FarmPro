74b426a6d513023f0cd0ef0d85e3b20f39cabd9b
Su-Yong <simssy2205@gmail.com>
2024-07-17 18:36:54 +0900

    feat(main): add ImageCard in MainPage

--- src/ui/common/Typography.tsx
+++ src/ui/common/Typography.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextProps } from 'react-native';
+import { StyleSheet, Text, TextProps, TextStyle } from 'react-native';
 
 import { createStyle } from '@/features/utils';
 import { Colors, TypographySheet } from '@/features/themes';
@@ -8,16 +8,18 @@ interface TypographyOptions {
   variant: keyof TypographySheet;
   color?: (color: Colors) => string;
   align?: 'left' | 'center' | 'right';
+  weight?: TextStyle['fontWeight'];
 }
-export const useTypographyStyle = createStyle((theme, { variant, align, color }: TypographyOptions) => ({
+export const useTypographyStyle = createStyle((theme, { variant, align, color, weight }: TypographyOptions) => ({
   ...theme.typography[variant],
   color: color?.(theme.colors),
   textAlign: align,
+  fontWeight: weight,
 }));
 
 export type TypographyProps = TextProps & Partial<TypographyOptions>;
-export const Typography = ({ variant = 'body1', color, align, ...props }: TypographyProps) => {
-  const style = useTypographyStyle({ variant, color, align });
+export const Typography = ({ variant = 'body1', color, align, weight, ...props }: TypographyProps) => {
+  const style = useTypographyStyle({ variant, color, align, weight });
 
   return (
     <Text
