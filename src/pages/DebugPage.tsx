3ca685d6b73931c5ebdd4d1cd7f202819f7b71ec
Su-Yong <simssy2205@gmail.com>
2024-07-17 18:09:12 +0900

    fix(ios): fix ios style

--- src/pages/DebugPage.tsx
+++ src/pages/DebugPage.tsx
@@ -3,14 +3,22 @@ import { KeyboardAvoidingView } from 'react-native-keyboard-controller';
 import Icon from 'react-native-vector-icons/Octicons';
 
 import { Button, TextInput, Typography } from '@/ui/common';
+import { useNavigation } from '@react-navigation/native';
 
 export const DebugPage = () => {
+  const navigation = useNavigation();
+
   return (
     <KeyboardAvoidingView
       behavior={'position'}
       style={{ flex: 1 }}
     >
       <View style={{ padding: 8 }}>
+        <Typography variant={'head1'}>Button</Typography>
+        <Button onPress={() => navigation.navigate('onBoarding')}>
+          go OnBoardPage
+        </Button>
+
         <Typography variant={'head1'}>Typography</Typography>
         <Typography variant={'head1'}>head1</Typography>
         <Typography variant={'head2'}>head2</Typography>
